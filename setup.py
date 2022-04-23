e31059f2d8eae4c0e63253f1481961c617a8ce4a
Travis Hamm <travis.m.hamm#gmail.com>
2022-04-23 14:05:18 -0700

    Branch fix.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="animal",
+    version="0.0.1dev",
+    install_requires=[
+        "numpy",
+        "torch>=1.11",
+        "gym",
+        "click",
+        "kindling @ git+https://github.com/jfpettit/kindling@main",
+        "tqdm"
+    ]
+)
